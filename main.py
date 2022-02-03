9f605d85eb53f1b2ec8626c588a4c4b2c70cd0ab
ve1alebe <93830935+ve1alebe@users.noreply.github.com>
2022-02-03 17:10:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-# This is a sample Python script.
+import os
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import requests
 
+map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.1,0.002&l=map"
+response = requests.get(map_request)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
 
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.blit(pygame.image.load(map_file), (0, 0))
+
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
+
 
